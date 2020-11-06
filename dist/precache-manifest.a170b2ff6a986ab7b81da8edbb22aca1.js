79a65b61611f9c316688eac65d521576f862d4e4
casper <orichcasper@gmail.com>
2020-11-06 17:46:17 +0300

    first build

---
+++ dist/precache-manifest.a170b2ff6a986ab7b81da8edbb22aca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd537baf9613e9af708",
+    "url": "/css/app.4a906a3c.css"
+  },
+  {
+    "revision": "3139ce296a613a2e20fe",
+    "url": "/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "98918a654eb3b6a8627cff264c3e06f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd537baf9613e9af708",
+    "url": "/js/app.39ca439d.js"
+  },
+  {
+    "revision": "3139ce296a613a2e20fe",
+    "url": "/js/chunk-vendors.29f5cda8.js"
+  },
+  {
+    "revision": "32edccd56d04bd0c956914ba927e9fb3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
